8e7c8d7c0e40aaf70a14df8df0c110d688e67020
Kay M <48743077+KayMM@users.noreply.github.com>
2022-07-19 10:05:26 -0700

    React changes to code.

--- ReactPractice/react-course/src/App.js
+++ ReactPractice/react-course/src/App.js
@@ -1,8 +1,15 @@
-
+import Todo from './components/Todo';
 
 function App() {
+
   return (
-    <div>Hello</div>
+      <div>
+          <h1>My Todos</h1>
+          <Todo text='Learn React'/>
+          <Todo text='Stuff'/>
+          <Todo text='Morestuff' />
+          
+      </div>
   );
 }
 
---
+++ ReactPractice/react-course/src/components/Todo.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import Modal from './Modal';
+import Backdrop from './Backdrop';
+
+function Todo(props) {
+    const [ modalIsOpen, setModalIsOpen ] = useState(false);
+
+    function deleteHandler() {
+        console.log('clicked');
+        console.log(props.text);
+        setModalIsOpen(true);
+
+    }
+
+
+    return (
+        <div className='card'>
+            <h2>{props.text}</h2>
+            <div className='actions'>
+                <button className='btn' onClick={deleteHandler}>Delete</button>
+            </div>
+            {modalIsOpen && <Modal />}
+            {modalIsOpen && <Backdrop onClick={closeModalHandler} />}
+         
+        </div>
+    );
+}
+
+export default Todo;
